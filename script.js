e29891ce99bac986a4ec18ddab485f699e0d9894
Mrinmay Santra <mrinmay7875@gmail.com>
2020-09-06 15:25:08 +0530

    Added CSS and Bootstrap styling

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 const taskInputField = document.querySelector(".task-input-field");
 const addTaskBtn = document.querySelector(".add-task-btn");
-const taskList = document.querySelector(".task-list");
 const deleteTaskBtn = document.querySelector(".clear-task-btn");
 
-// Add new task Button
+const taskList=document.querySelector(".task-list")
 
 let taskArray = [];
 function addNewTask() {
+  // Fetch the new task from Input value
   let newTask = taskInputField.value;
+  // Create an li element for the new task and 
+  // assign the className
   let newListItemElement = document.createElement("li");
   newListItemElement.className = "task-list-item";
+  // Create a text node and append into the <p> element</p>
   let newTextNode = document.createTextNode(newTask);
-  newListItemElement.appendChild(newTextNode);
+  let newPelement=document.createElement("span");
+  newPelement.appendChild(newTextNode);
+  newPelement.className="newTaskContent"
+ 
+  newListItemElement.appendChild(newPelement);
 
-  taskList.appendChild(newListItemElement);
+  // Create delete icon and append into list item element
+let deleteItem=document.createElement("i");
+  deleteItem.className ="far fa-trash-alt fa-sm delete-icon"
+newListItemElement.appendChild(deleteItem);
+
+// Append new task element in the <UL>
+taskList.appendChild(newListItemElement);
+
+
+  addDeleteEvents();
 }
+// Add new task addEventListener
 addTaskBtn.addEventListener("click", addNewTask);
 
 // Delete All task button
-function deleteTasks() {
+function deleteAllTasks() {
   taskList.innerHTML = "";
 }
+//Delete all task eventlistener
+deleteTaskBtn.addEventListener("click", deleteAllTasks);
 
-deleteTaskBtn.addEventListener("click", deleteTasks);
+
+
+// function to add delete events to all delete icons
+function addDeleteEvents(){
+  let  allDeleteIcons=document.querySelectorAll(".delete-icon")
+  allDeleteIcons=[...allDeleteIcons]
+  allDeleteIcons.forEach(item=>{
+    item.addEventListener("click",deleteSingleTask);
+  })
+}
+
+//Delete a singLisle task
+function deleteSingleTask(event){
+  // console.log(
+  //   event.target.parentElement);
+event.target.parentElement.remove();
+
+}
